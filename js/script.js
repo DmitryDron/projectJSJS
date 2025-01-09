cd5c28e034bbcf46635490c61e3147dfd815f2f3
Dima <dima.golyy@gmail.com>
2025-01-09 22:08:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let userChoice = confirm("Ты долбоеб и чо хуй?")
+
+if (userChoice == true) {
+    window.location.href = "https://www.dream-singles.com/";
+} else {
+    alert("Ну и чо хуй");
+}
